ac57867e754ec9a42a9c061ce8ed4bc2e4eb65a3
SemanticBeeng <nick@semanticbeeng.com>
2019-03-18 10:09:08 -0700

    tests green

--- src/main/java/com/ing/baker/tutorials/WebShopRecipe.java
+++ src/main/java/com/ing/baker/tutorials/WebShopRecipe.java
@@ -1,7 +1,13 @@
 package com.ing.baker.tutorials;
 
 import com.ing.baker.recipe.javadsl.Recipe;
+import com.ing.baker.tutorials.interactions.ManufactureGoods;
+import com.ing.baker.tutorials.interactions.SendInvoice;
+import com.ing.baker.tutorials.interactions.ShipGoods;
 import com.ing.baker.tutorials.interactions.ValidateOrder;
+import com.ing.baker.tutorials.interactions.events.SensoryEvents;
+import com.ing.baker.tutorials.interactions.events.ShipGoodsEvents;
+import com.ing.baker.tutorials.interactions.events.ValidateOrderEvents;
 
 import static com.ing.baker.recipe.javadsl.InteractionDescriptor.of;
 
@@ -9,6 +15,16 @@ class WebShopRecipe {
     static Recipe getRecipe() {
         //TODO implement the recipe according to the sequence diagram in docs/sequence.png
         return new Recipe("WebShop")
-                .withInteraction(of(ValidateOrder.class));
+                .withSensoryEvents(
+                    SensoryEvents.OrderPlaced.class,
+                    SensoryEvents.CustomerInfoReceived.class,
+                    SensoryEvents.PaymentMade.class)
+                .withInteractions(
+                    of(ValidateOrder.class),
+                    of(ManufactureGoods.class)
+                        .withRequiredEvents(SensoryEvents.PaymentMade.class, ValidateOrderEvents.OrderValid.class),
+                    of(ShipGoods.class),
+                    of(SendInvoice.class)
+                        .withRequiredEvents(ShipGoodsEvents.GoodsShipped.class));
     }
 }
